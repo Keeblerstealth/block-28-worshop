ecc9156bbff2da35f8f4865efca20960829dd1cb
devin <devin.lighgheart@gmail.com>
2024-11-05 11:20:05 -0700

    colors up and running

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import { Routes, Route, } from "react-router-dom";
+import Red from "./components/Red";
+import Blue from "./components/Blue";
+import { Link } from "react-router-dom";
 
-function App() {
+const App = () => {
 
   return (
     <>
+      <div id="container">
+
+        <div id="navbar">
+          <Link to={"/"}>Home</Link>
+          <Link to={"/blue"}>blue</Link>
+          <Link to={"/red"}>red</Link>
+        </div>
+        <div id="main-section">
+          <Routes>
+            <Route path="/" element={<h2>Home</h2>} />
+            <Route path="/blue" element={<Blue/>} />
+            <Route path="/red" element={<Red/>} />
+          </Routes>
+        </div>
+      </div>
 
     </>
   )
